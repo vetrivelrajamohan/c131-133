8bf88cf330df79b0a2b713929b893fb2376e6e2d
vetrivelrajamohan <91267642+vetrivelrajamohan@users.noreply.github.com>
2022-07-27 19:39:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+}
+
+function modelLoaded() 
+{
+    console.log("model loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults()
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() 
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status != "" )
+    {
+        for (var i = 0; i < objects.length; i++) 
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + '%', objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
